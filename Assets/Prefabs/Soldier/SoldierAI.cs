e0744ca350d8adff60e10bfefd827d143ec71b04
SilverboltGames <61442059+SilverboltGames@users.noreply.github.com>
2020-04-22 00:05:05 +0600

    Adding(!) Soldier, fixed default mat
    So I had previously created a default material to drag and drop because I didn't know how to revert a gameobject back to use default, apparenly you just have to select the default from the material field of the sprite renderer(I guess mesh renderer for 3d?)

---
+++ Assets/Prefabs/Soldier/SoldierAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierAI : MonoBehaviour
+{
+    [SerializeField] private float _detectionRadius = 10f;
+    [SerializeField] private Transform _target = default;
+
+	void Update()
+	{
+        if (Vector2.Distance(transform.position, _target.position) < _detectionRadius)
+        {
+            //walk left and raycast, shoot if you detect target in line of sight
+            //stand, walk left, shoot straight, shoot up
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _detectionRadius);
+    }
+}
