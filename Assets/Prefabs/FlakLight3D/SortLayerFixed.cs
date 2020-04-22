092ca009736f92260e4b2fb3064a087974ae0efe
SilverboltGames <61442059+SilverboltGames@users.noreply.github.com>
2020-04-23 02:23:37 +0600

    Added flakLight and flakLight3D

---
+++ Assets/Prefabs/FlakLight3D/SortLayerFixed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortLayerFixed : MonoBehaviour
+{
+    [SerializeField] private string sortingLayerName = string.Empty; //initialization before the methods
+    [SerializeField] private int orderInLayer = 0;
+    private Renderer MyRenderer;
+    private void Start()
+    {
+        MyRenderer = GetComponent<Renderer>();
+        if (sortingLayerName != string.Empty)
+        {
+            MyRenderer.sortingLayerName = sortingLayerName;
+            MyRenderer.sortingOrder = orderInLayer;
+        }
+    }
+}
