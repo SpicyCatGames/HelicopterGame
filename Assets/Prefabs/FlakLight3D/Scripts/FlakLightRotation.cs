4df020416878460b281843fc151df7832ad5a844
SilverboltGames <61442059+SilverboltGames@users.noreply.github.com>
2020-04-23 22:41:11 +0600

    Added a way to change sorting layer of 3D flak which has no sprite renderer

---
+++ Assets/Prefabs/FlakLight3D/Scripts/FlakLightRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlakLightRotation : MonoBehaviour
+{
+    [SerializeField] private Transform _lightCase = default;
+    [SerializeField] private float _zRotationOffset = -90f;
+
+    [SerializeField] private Light _light;
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, _lightCase.rotation.eulerAngles.z + _zRotationOffset);
+    }
+}
