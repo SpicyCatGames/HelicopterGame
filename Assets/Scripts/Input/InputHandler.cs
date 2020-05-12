1589287010afcec529badfa72007898a2a906200
SilverboltGames <61442059+SilverboltGames@users.noreply.github.com>
2020-05-12 23:47:52 +0600

    Added touch controls and locked to landscape

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -11,9 +11,16 @@ namespace customInputs
         public static float Vertical { get; private set; }
         public static bool SpaceKey { get; private set; }
 
+        #region TouchInput fields
+        private Vector2 _startTouchPosition;
+        [SerializeField] [Range(0, 1)] private float radiusSizeMultiplier = 1; //how much of the display height to be used as ui joystick radius
+        #endregion
+
         void Update()
         {
             KeyboardInput();
+            //MouseInput(); This is just an easy way to debug touch
+            TouchInput();
         }
 
         void KeyboardInput()
@@ -21,6 +28,60 @@ namespace customInputs
             Horizontal = Input.GetAxisRaw("Horizontal");
             Vertical = Input.GetAxisRaw("Vertical");
             SpaceKey = Input.GetKey(KeyCode.Space);
+        }
+
+        void MouseInput()
+        {
+            float scalingDivisor = Screen.height * radiusSizeMultiplier;
+            if (Input.GetMouseButtonDown(0)) //equivalent to touch start
+            {
+                _startTouchPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0)) //equivalent to touch continuing
+            {
+                float horizontalUnscaled = Input.mousePosition.x - _startTouchPosition.x;
+                float horizontalScaled = horizontalUnscaled / scalingDivisor;
+                Horizontal = (horizontalScaled < 1) ? horizontalScaled : 1;
+
+                float verticalUnscaled = Input.mousePosition.y - _startTouchPosition.y;
+                float verticalScaled = verticalUnscaled / scalingDivisor;
+                Vertical = (verticalScaled < 1) ? verticalScaled : 1;
+            }
+            else if (Input.GetMouseButtonUp(0)) //equivalent to touch release
+            {
+                Horizontal = 0;
+                Vertical = 0;
+            }
+        }
+
+        void TouchInput()
+        {
+            if (Input.touchCount > 0)
+            {
+                float scalingDivisor = Screen.height * radiusSizeMultiplier;
+                Touch touch = Input.GetTouch(0);
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        _startTouchPosition = touch.position;
+                        break;
+
+                    case TouchPhase.Moved:
+                        float horizontalUnscaled = touch.position.x - _startTouchPosition.x;
+                        float horizontalScaled = horizontalUnscaled / scalingDivisor;
+                        Horizontal = (horizontalScaled < 1) ? horizontalScaled : 1;
+
+                        float verticalUnscaled = touch.position.y - _startTouchPosition.y;
+                        float verticalScaled = verticalUnscaled / scalingDivisor;
+                        Vertical = (verticalScaled < 1) ? verticalScaled : 1;
+                        break;
+
+                    case TouchPhase.Ended:
+                        Horizontal = 0;
+                        Vertical = 0;
+                        break;
+                }
+            }
         }
     }
 }
