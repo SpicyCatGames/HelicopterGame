3375c8baa0c788074320afe9c46c1fc5b4817c3c
Md Sadman Chowdhury <61442059+SilverboltGames@users.noreply.github.com>
2020-05-25 17:46:37 +0600

    Minor fixes and touch implemented (#6)
    * SilverUtils, Helisprites
    
    *Added some useful functions to the utils class
    *Edited out the gun from helicopter sprite to make it a seperate sprite because the gun will soon be made to rotate seperately
    
    * Completed SilverUtils.cs for now

--- Assets/Scripts/Utils/SilverUtils.cs
+++ Assets/Scripts/Utils/SilverUtils.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+
 namespace SilverUtils.Angle
 {
     public static class Degrees
@@ -11,47 +12,96 @@ namespace SilverUtils.Angle
         {
             return (angleInDegrees < 0) ? (angleInDegrees + 360) : angleInDegrees;
         }
-        public static Vector3 GetEditorRotations(Transform transform)//please ignore this as well
+
+        public static float Vec2toDeg(Vector2 direction)
         {
-            Vector3 angle = transform.eulerAngles;
-            float x = angle.x;
-            float y = angle.y;
-            float z = angle.z;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            return (angle < 0) ? (angle + 360) : angle;
+        }
+        public static float Vec2toDeg(Vector2 from, Vector2 to) => Vec2toDeg(to - from);
 
-            if (Vector3.Dot(transform.up, Vector3.up) >= 0f)
+        /// <summary>
+        /// Values may be a number very close but not enough for Mathf.Approximately()
+        /// </summary>
+        public static Vector2 DegtoVec2(float angleZ)
+        {
+            //some statements for 90, 180 , 270 to avoid accuracy loss from float
+            /*if(Mathf.Approximately(Mathf.Abs(angleZ % 90), 0))
             {
-                if (angle.x >= 0f && angle.x <= 90f)
-                {
-                    x = angle.x;
-                }
-                if (angle.x >= 270f && angle.x <= 360f)
-                {
-                    x = angle.x - 360f;
-                }
+                angleZ = Normalizeto360(angleZ);
+                if (Mathf.Approximately(angleZ, 90)) return new Vector2(0, 1);
+                if (Mathf.Approximately(angleZ, 180)) return new Vector2(-1, 0);
+                if (Mathf.Approximately(angleZ, 270)) return new Vector2(0, -1);
+            }*/
+            float angleRad = angleZ * Mathf.Deg2Rad;
+            return new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+        }
+        /*public static Vector2 DegtoVec2(float angleZ)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, -angleZ);
+            return rotation * new Vector2(0, 1); 
+        }*/
+
+        public static float Normalizeto360(float angle)
+        {
+            if (angle >= 360)
+            {
+                return angle % 360;
             }
-            if (Vector3.Dot(transform.up, Vector3.up) < 0f)
+            if (angle < 0)
             {
-                if (angle.x >= 0f && angle.x <= 90f)
-                {
-                    x = 180 - angle.x;
-                }
-                if (angle.x >= 270f && angle.x <= 360f)
-                {
-                    x = 180 - angle.x;
-                }
+                /*angle = Mathf.Abs(angle);
+                angle %= 360;
+                angle = 360 - angle;*/
+                return 360 - (Mathf.Abs(angle) % 360);
+            }
+            return angle;
+        }
+
+        /// <summary>
+        /// Returns -1, 1 , whichever to add to get to target rotation quickest
+        /// Values must be between 0 and 360
+        /// </summary>
+        public static int RotationDirection(float fromEuler, float toEuler)
+        {
+            //if difference between the two rotations is more than 180, the closest way to the target is through 360 wraparound
+            if(Mathf.Abs(fromEuler - toEuler) < 180)
+            {
+                return (toEuler > fromEuler) ? 1 : -1;
+            }
+            return (toEuler > fromEuler) ? -1 : 1;
+        }
+
+        /// <summary>
+        /// Clamp while taking into account the 360 wraparound
+        /// </summary>
+        public static float ClampRotation(float eulerAngle, float fromEuler, float toEuler)
+        {
+            eulerAngle = Normalizeto360(eulerAngle);
+
+            if (fromEuler < toEuler)
+            {
+                return Mathf.Clamp(eulerAngle, fromEuler, toEuler);
             }
 
-            if (angle.y > 180)
+            //else if from is greater than to
+            //if it's between the two numbers, clamp to the closest one.
+            if ((eulerAngle < fromEuler) && (eulerAngle > toEuler))
             {
-                y = angle.y - 360f;
+                return ((fromEuler - eulerAngle) < (eulerAngle - toEuler)) ? fromEuler : toEuler;
             }
 
-            if (angle.z > 180)
-            {
-                z = angle.z - 360f;
-            }
-            return new Vector3(x, y, z);
+            return eulerAngle;
+        } 
+    }
+}
+
+namespace SilverUtils
+{
+    public static class Misc{
+        public static float GetPPU(Camera cam)
+        {
+            return (Screen.height / 2) / cam.orthographicSize;
         }
     }
-
 }
