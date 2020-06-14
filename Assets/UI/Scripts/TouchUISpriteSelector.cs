5bac445676c2ad44e3edf4a343e76d34aee9e934
SilverboltGames <61442059+SilverboltGames@users.noreply.github.com>
2020-06-14 08:03:53 +0600

    Touch Joystick implemented, no visual or jump button yet

---
+++ Assets/UI/Scripts/TouchUISpriteSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchUISpriteSelector : MonoBehaviour
+{
+    //this script will select the sprite for touch joystick at any given time
+
+	void Update()
+	{
+		
+	}
+}
