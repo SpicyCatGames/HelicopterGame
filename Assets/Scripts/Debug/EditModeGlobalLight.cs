3e18723f62a46c53139acdb3a9fc8ec5444f56c2
Md Sadman Chowdhury <61442059+SilverboltGames@users.noreply.github.com>
2020-07-26 15:40:49 +0600

    Added a script to enable different global light in edit mode becoz it was too dark to see while building maps

---
+++ Assets/Scripts/Debug/EditModeGlobalLight.cs
@@ -0,0 +1,29 @@
+﻿//this script is for things that won't exist in play more or in build
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[ExecuteInEditMode]
+public class EditModeGlobalLight : MonoBehaviour
+{
+    public Light2D InGameLight2D;
+    public Light2D EditModeLight2D;
+
+    private void Update()
+    {
+        if (EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            EditModeLight2D.enabled = false;
+            InGameLight2D.enabled = true;
+        }
+        else
+        {
+            InGameLight2D.enabled = false;
+            EditModeLight2D.enabled = true;
+        }
+    }
+}
+#endif
