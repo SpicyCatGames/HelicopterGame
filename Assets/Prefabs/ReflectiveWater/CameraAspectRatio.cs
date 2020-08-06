181db3e184f8787bb3c3c9562a2f50f5568bde93
SpicyCatGames <61442059+SilverboltGames@users.noreply.github.com>
2020-08-06 23:58:10 +0600

    Added script to set aspect ratio of camera according to render texture

---
+++ Assets/Prefabs/ReflectiveWater/CameraAspectRatio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraAspectRatio : MonoBehaviour
+{
+    private Camera _cam;
+    [SerializeField] private RenderTexture _tex = default;
+    private void Start()
+    {
+        _cam = GetComponent<Camera>();
+        _cam.aspect = _tex.width / _tex.height;
+    }
+}
